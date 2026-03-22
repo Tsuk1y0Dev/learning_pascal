4eaa5a83983160355936a127828fafe208faeaf5
Tsuk1y0Dev <matpol050110@gmail.com>
2026-03-22 22:19:35 +0900

    worse code

---
+++ first_program/firstprogram.pas
@@ -0,0 +1,11 @@
+
+Program Styx;
+begin
+  writeln('Whose woods these are i think I know');
+  readln;
+  writeln('His house is in the village though');
+  readln;
+  writeln('He will not see me stopping here');
+  readln;
+  writeln('To watch his woods fill up with show');
+end.
