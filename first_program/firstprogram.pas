1b025e55971ff0ed90cc9c6a12da2a67cfa21830
Tsuk1y0Dev <matpol050110@gmail.com>
2026-03-22 22:20:29 +0900

    worse code: fix

--- first_program/firstprogram.pas
+++ first_program/firstprogram.pas
@@ -1,11 +1,11 @@
 
 Program Styx;
 begin
-  writeln('Whose woods these are i think I know');
+  write('Whose woods these are i think I know');
   readln;
-  writeln('His house is in the village though');
+  write('His house is in the village though');
   readln;
-  writeln('He will not see me stopping here');
+  write('He will not see me stopping here');
   readln;
-  writeln('To watch his woods fill up with show');
+  write('To watch his woods fill up with show');
 end.
