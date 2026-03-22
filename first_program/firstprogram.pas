ecee1e8d0499ab8bc713c8569ef04c3bb9216a1c
Tsuk1y0Dev <matpol050110@gmail.com>
2026-03-22 22:40:50 +0900

    Hello, world!

--- first_program/firstprogram.pas
+++ first_program/firstprogram.pas
@@ -1,11 +1,12 @@
 
-Program Styx;
-begin
-  write('Whose woods these are i think I know');
-  readln;
-  write('His house is in the village though');
-  readln;
-  write('He will not see me stopping here');
-  readln;
-  write('To watch his woods fill up with show');
-end.
+Program Hello20;
+
+Var 
+  n: shortint;
+Begin
+  While n < 20 Do
+    Begin
+      writeln('Hello, world!');
+      n := n + 1;
+    End
+End.
